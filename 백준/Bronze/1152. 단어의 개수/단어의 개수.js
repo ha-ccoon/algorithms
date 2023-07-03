dfce5994e160d459cd08feba1fa5badbfac5fe6c
Minha Sohn <86749331+ha-ccoon@users.noreply.github.com>
2023-07-04 06:51:42 +0900

    [Bronze II] Title: 단어의 개수, Time: 156 ms, Memory: 18004 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.js
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -1,13 +1,6 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString();
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin');
+const switchToArr = input.toString().trim().split(' ');
+const removeZeroException = switchToArr[0] === '' ? 0 : switchToArr.length;
 
-let wordsArr = input.trim().split(' '); 
-let countOfWords = 0;
-
-for (let i = 0; i < wordsArr.length; i++) {
-  if (wordsArr[i] !== '') {
-    countOfWords++;
-  }
-}
-
-console.log(countOfWords);
+console.log(removeZeroException);
