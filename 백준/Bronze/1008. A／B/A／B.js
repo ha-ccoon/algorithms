0fb911aa0f035199d54607a86b72995882d23657
Minha Sohn <86749331+ha-ccoon@users.noreply.github.com>
2023-07-03 09:02:02 +0900

    [Bronze V] Title: A/B, Time: 120 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('/dev/stdin');
+const switchToStr = input.toString().split(' ');
+const a = parseInt(switchToStr[0]);
+const b = parseInt(switchToStr[1]);
+
+console.log(a / b);
